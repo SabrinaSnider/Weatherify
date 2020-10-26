7862d10cbe2a823a3d50db896ce155e7563818ef
Sabrina Snider <sabrina.snider10@gmail.com>
2020-10-26 00:36:39 -0400

    Added nodemon for hot refreshing of server.

--- client/src/app/spotify-auth/spotify-auth.component.ts
+++ client/src/app/spotify-auth/spotify-auth.component.ts
@@ -1,27 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
-import { Router, RouterModule, Routes } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-spotify-auth',
-  templateUrl: './spotify-auth.component.html',
-  styleUrls: ['./spotify-auth.component.scss']
+  selector: "app-spotify-auth",
+  templateUrl: "./spotify-auth.component.html",
+  styleUrls: ["./spotify-auth.component.scss"],
 })
 export class SpotifyAuthComponent implements OnInit {
-
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient) {}
 
   hello(): void {
     console.log("hello!");
-    this.http.get("http://localhost:3000/api/spotify/login").subscribe((data) => {
-      console.log(data, data["redirect"])
-      // router.navigate([data.]);
-    })
+    this.http
+      .get("http://localhost:3000/api/spotify/auth")
+      .subscribe((json) => console.log(json));
   }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
